c202cec895d72fd3f9c0eae0c2502b4527919341
Adam Roberts <aroberts85@yahoo.com>
2017-06-12 12:44:16 -0400

    add devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  
   def index
     @posts = Post.all.order('created_at DESC')
   end
@@ -33,7 +35,7 @@ class PostsController < ApplicationController
       render 'edit'
     end
   end
-  
+
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
